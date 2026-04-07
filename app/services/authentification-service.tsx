ff0f0a316e4a775e2590a4f77dd09ffc6f067fcf
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-04-07 13:09:34 +0200

    feat(login): added login function

--- app/services/authentification-service.tsx
+++ app/services/authentification-service.tsx
@@ -3,10 +3,16 @@ import { User } from "~/types/user"
 
 export class AuthentificationService {
     private baseUrl = "http://localhost:8000"
+    private usersUrl = "users"
     private authUrl = "auth"
     private registerUrl = "register"
+    private loginUrl = "login"
 
     register(user: User) {
         return axios.post(this.baseUrl + "/" + this.authUrl + "/" + this.registerUrl + "/", user)
+    }
+
+    login(user: UserLogin) {
+        return axios.post(this.baseUrl + "/" + this.authUrl + "/" + this.loginUrl + "/", user)
     }
 }
