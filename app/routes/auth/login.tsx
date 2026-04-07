fa1909f752386755a69d01c00f95f1a6ebb25ebc
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-04-07 13:26:56 +0200

    feat(login): added login page

---
+++ app/routes/auth/login.tsx
@@ -0,0 +1,40 @@
+import type { Route } from "./+types/register";
+
+import { AuthentificationService } from "~/services/authentification-service";
+import { UserLogin } from "~/types/userLogin";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Register" },
+    { name: "description", content: "Register Page" },
+  ];
+}
+
+let authentificationService: AuthentificationService = new AuthentificationService
+
+export default function Login() {
+
+  const login = (event: any) => {
+    event.preventDefault()
+
+    const email = event.target.email.value
+    const password = event.target.password.value
+    const newUser = new UserLogin(email, password)
+
+    authentificationService.login(newUser).then(response => {
+      console.log(response)
+    })
+  }
+
+  return (
+    <div>
+      <h2>Login to your account</h2> 
+      <form className="formClass" onSubmit={login}>
+        <input className="formInput" id="email" type="email" placeholder="Email Address" required/>
+        <input className="formInput" id="password" type="password" placeholder="Password" required/>
+        
+        <button className="submitButton" type="submit">Submit</button>
+      </form>
+  </div>
+  )
+}
