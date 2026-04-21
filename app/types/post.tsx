367485c98db57ab390bb1e6c4fe07d182c20fc32
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-04-21 16:34:41 +0200

    feat(class): added category and post classes

---
+++ app/types/post.tsx
@@ -0,0 +1,21 @@
+import { User } from "./user"
+import { Category } from "./category"
+
+export class Post {
+    id: number
+    creator: User
+    title: string
+    text: string
+    active: boolean
+    creation_date: string
+    categories: Array<Category>
+
+    constructor(creator: User, title: string, text: string, categories: Array<Category>) {
+        this.creator = creator
+        this.title = title
+        this.text = text
+        this.active = true
+        this.creation_date = new Intl.DateTimeFormat("en-CA").format(new Date())
+        this.categories = categories
+    }
+}
