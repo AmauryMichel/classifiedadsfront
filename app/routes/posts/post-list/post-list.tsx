dae2406cc97078aefa682b83228c242e78de94a8
AmauryMichel <56653816+AmauryMichel@users.noreply.github.com>
2026-05-06 18:47:15 +0200

    feat(post): added category filter to post list

--- app/routes/posts/post-list/post-list.tsx
+++ app/routes/posts/post-list/post-list.tsx
@@ -3,36 +3,74 @@ import type { Route } from "./+types/post-list";
 import { Post } from "~/types/post";
 
 import { UserService } from "~/services/user-service";
+import { CategoryService } from "~/services/category-service";
 import { useEffect, useState } from "react";
+import type { Category } from "~/types/category";
+import { Link } from "react-router";
 
-export function meta({}: Route.MetaArgs) {
+import './post-list.css'
+
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Post List" },
     { name: "description", content: "Post list" },
   ];
 }
 
-let userService: UserService = new UserService
+let userService: UserService = new UserService()
+let categoryService: CategoryService = new CategoryService()
 
 export default function PostList() {
 
   const [posts, setPosts] = useState<Post[]>([])
+  const [allPosts, setAllPosts] = useState<Post[]>([])
+  const [categories, setCategories] = useState<Category[]>([])
+
+  function filterPosts(event: any) {
+    if (event.target.value === "") {
+      setPosts(allPosts)
+    } else {
+      const categoryId = Number.parseInt(event.target.value)
+      setPosts(allPosts.filter((post) => post.categories.includes(categoryId)))
+    }
+  }
 
   useEffect(() => {
     const userId = Number(localStorage.getItem('userId'))
+
     userService.getUserPosts(userId).then(response => {
       setPosts(response.data)
+      setAllPosts(response.data)
+    })
+
+    categoryService.getCategories().then(response => {
+      setCategories(response.data.results)
     })
   }, [])
 
   return (
-    <div className="postList">
-      {posts.map(p => (
-        <div className="post" key={p.id}>
-          <b>{p.title}</b><br />
-          {p.text}
-        </div>
-      ))}
-    </div>
+    <>
+      <div id="filter">
+        Filter by category:
+        <select id="categoryFilter" onChange={filterPosts}>
+          <option value="">No filter</option>
+          {categories.map(category => (
+            <option key={category.id} value={category.id}>{category.name}</option>
+          ))}
+        </select>
+      </div>
+
+      <div id="postList">
+        {posts.map(post => (
+          <Link to={`/posts/${post.id}`}>
+            <div className="post" key={post.id}>
+              <b>{post.title}</b>
+              <hr />
+              {post.text}
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
   )
 }
